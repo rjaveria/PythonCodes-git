a50ad7649852263d503b0cc611b2d5355cdd22f9
jiyaa ms <jiyamscs1489@gmail.com>
2024-06-26 23:21:26 +0500

    calendar.py added

---
+++ arithmeticoperations.py
@@ -0,0 +1,11 @@
+num1=int(input("Enter First Number:"))
+num2=int(input("Enter Second Number:"))
+sum=num1 + num2
+print("The addition of two numbers is :",sum)
+sub=num1 - num2
+print("The subtraction of two numbers is:",sub)
+mul=num1 * num2
+print("The multiplication of two numbers is:",mul)
+div=int(num1) // int(num2)
+print("The division of two numbers is:",div)
+
